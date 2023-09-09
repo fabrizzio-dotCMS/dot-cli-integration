3a8d764bef26266efc098f78925ef48bfac1cb66
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2023-09-08 21:42:25 -0600

    testing action

--- actions/dot-cli/entrypoint.sh
+++ actions/dot-cli/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
   #bash "./dot-cli/run-java.sh" "$@"
-  var=$(bash ./dot-cli/run-java.sh "$@" )
+
+  ls /dot-cli
+  
+  var=$(bash /dot-cli/run-java.sh "$@" )
   echo "var: $var"
   echo "exit code: $?"
