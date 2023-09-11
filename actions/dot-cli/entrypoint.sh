2a077e278b93f791ea6fb896dc15599aa5c12e35
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2023-09-11 16:17:31 -0600

    fix replacing token

--- actions/dot-cli/entrypoint.sh
+++ actions/dot-cli/entrypoint.sh
@@ -2,22 +2,19 @@
   
   ls /dot-cli
 
-  echo "config::::"
-
   #proof that we can access the repo from within the script
   cat /github/workspace/README.md
   
 
-  tkn="$1"
-  echo "Safe pass is :: $tkn"  
+  #tkn="$1"
+  #echo "Safe pass is :: $tkn"  
 
   #Replace the safe-pass with the value passed in the docker workflow action
-  sed -i -e  "s/plchldr/$tkn/g" "${DOT_SERVICES_HOME}dot-service.yml"
+  #sed -i -e  "s/plchldr/$tkn/g" "${DOT_SERVICES_HOME}dot-service.yml"
 
-
-  echo "$@";  # will echo all args
-  shift;  # will remove first arg from the "$@"
-  echo "$@";  # will echo all args except first one
+  #echo "$@";  # will echo all args
+  #shift;  # will remove first arg from the "$@"
+  #echo "$@";  # will echo all args except first one
 
   var=$(bash /dot-cli/run-java.sh "$@" )  
   echo "var: $var"
